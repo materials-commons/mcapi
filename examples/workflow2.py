19f4c73bea156e90a2031a7e6ef292504b2d5024
Terry Weymouth <weymouth@umich.edu>
2016-11-03 22:43:50 -0400

    Updated workflow2 example

--- examples/workflow2.py
+++ examples/workflow2.py
@@ -1,18 +1,36 @@
-# Not implemented
+import datetime
+from mcapi import create_project, Template, get_remote_config_url
 
+print "This example is running on the REST interface at " + get_remote_config_url()
+print "Using the configuration (config.json) in ~/.materialscommons/"
+
+def fake_name(prefix):
+    number="%05d" % randint(0,99999)
+    return prefix+number
+
+project_name = "API-Project"
+experiment_name = fake_name("Experiment-")
+
+print "Creating experiment, " + experiment_name + " in project " + project_name
+
+project_description = "a test project generated from api"
+experiment_description = "a test experiment generated from api"
+sample_name = 'Test Sample 1'
+process1_name = "Create Simulation Sample"
+process2_name = "Monte Carlo Simulation"
+
+## the workflow ##
 project = create_project(
-        name = "ThisProject",
-        description = "is amazing")
+    name=project_name,
+    description=project_description)
 
 experiment = project.create_experiment(
-        name = "Experiment 1",
-        description = "a test experiment generated from api")
+    name=experiment_name,
+    description=experiment_description)
 
-processA = experiment.create_process_from_template(Template.create_sample)
-sample = create_sample_process.create_samples(
-    sample_names = ['Test Sample 1']
-    )[0]
+create_sample_process = experiment.create_process_from_template(Template.create)
+sample = create_sample_process.create_samples([sample_name])[0]
 
-processB = experiment.\
-    create_process_from_template(Template.computation).\
+compute_process = experiment. \
+    create_process_from_template(Template.compute). \
     add_samples_to_process([sample])
