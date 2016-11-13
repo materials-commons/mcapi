c2b9ab7e9d441321e49c4cf7998476ecd8cbc863
Terry Weymouth <weymouth@umich.edu>
2016-11-13 12:22:35 -0500

    Updates code to remove PIP 8 errors

--- mcapi/api.py
+++ mcapi/api.py
@@ -5,28 +5,27 @@ import magic
 
 # Defaults
 _mcorg = Remote()
-_remote = None
+_current_remote = None
 
 
 def use_remote():
-    if (_remote): return _remote
+    global _current_remote
+    if _current_remote:
+        return _current_remote
     return mcorg()
 
 
 def set_remote(remote):
-    _remote = remote
+    global _current_remote
+    _current_remote = remote
 
 
 def unset_remote():
-    _remote = None
+    global _current_remote
+    _current_remote = None
 
 
 def mcorg():
-    """
-    Returns
-    ---------
-      Default Remote: Materials Commons at 'https://materialscommons.org/api'
-    """
     return _mcorg
 
 
@@ -89,9 +88,11 @@ def create_project(name, description, remote=use_remote()):
     }
     return post(remote.make_url("projects"), data)
 
-def fetch_project(id, remote=use_remote()):
-    api_url = "projects/" + id
+
+def fetch_project(project_id, remote=use_remote()):
+    api_url = "projects/" + project_id
     return get(remote.make_url_v2(api_url), remote=remote)
+
 
 # Experiment
 
@@ -111,32 +112,37 @@ def create_experiment(project_id, name, description, remote=use_remote()):
     }
     return post(remote.make_url_v2("projects/" + project_id + "/experiments"), data)
 
+
 # Process
 
 def create_process_from_template(project_id, experiment_id, template_id, remote=use_remote()):
     data = {
-        "id" : template_id
+        "id": template_id
     }
     api_url = "projects/" + project_id + \
         "/experiments/" + experiment_id + \
         "/processes/templates/" + template_id
     return post(remote.make_url_v2(api_url), data)
 
+
 # Sample
 
 def create_samples(project_id, process_id, sample_names, remote=use_remote()):
-    sample_names_data = [{"name":name} for name in sample_names]
+    sample_names_data = [{"name": name} for name in sample_names]
     data = {
-        "process_id" : process_id,
-        "samples" : sample_names_data
+        "process_id": process_id,
+        "samples": sample_names_data
     }
     api_url = "projects/" + project_id + "/samples"
     return post(remote.make_url_v2(api_url), data)
 
 # Create sample process
 
+
 def add_samples_to_process(project_id, experiment_id, process, samples, remote=use_remote()):
-    samples_data = map((lambda s: {'command': 'add', 'id':s.id, 'property_set_id': s.property_set_id}),samples)
+    samples_data = map(
+        (lambda s: {'command': 'add', 'id': s.id, 'property_set_id': s.property_set_id}),
+        samples)
     data = {
         "template_id": process.template_id,
         "process_id": process.id,
@@ -147,6 +153,7 @@ def add_samples_to_process(project_id, experiment_id, process, samples, remote=u
               "/processes/" + process.id
     return put(remote.make_url_v2(api_url), data)
 
+
 # directory
 
 def directory_by_id(project_id, directory_id, remote=use_remote()):
@@ -154,7 +161,8 @@ def directory_by_id(project_id, directory_id, remote=use_remote()):
               "/directories/" + directory_id
     return get(remote.make_url_v2(api_url))
 
-def directory_by_path(project_id, directory_id,path, remote=use_remote()):
+
+def directory_by_path(project_id, directory_id, path, remote=use_remote()):
     data = {
         "from_dir": directory_id,
         "path": path
@@ -163,18 +171,20 @@ def directory_by_path(project_id, directory_id,path, remote=use_remote()):
               "/directories/"
     return post(remote.make_url_v2(api_url), data)
 
+
 # file
 
 def file_upload(project_id, directory_id, file_name, input_path, remote=use_remote()):
     with open(input_path, 'rb') as f:
-        api_url = "projects/" + project_id + "/directories/" + directory_id +"/fileupload"
+        api_url = "projects/" + project_id + "/directories/" + directory_id + "/fileupload"
         mime_type = magic.Magic(mime=True).from_file(input_path)
-        files = {'file': (file_name,f,mime_type)}
+        files = {'file': (file_name, f, mime_type)}
         restpath = remote.make_url_v2(api_url)
         r = requests.post(restpath, params=remote.params, files=files)
         if r.status_code == requests.codes.ok:
             return r.json()
         r.raise_for_status()
+
 
 def file_download(project_id, file_id, output_file_path, remote=use_remote()):
     with open(output_file_path, 'wb') as f:
@@ -190,8 +200,10 @@ def file_download(project_id, file_id, output_file_path, remote=use_remote()):
 
     return output_file_path
 
+
 def set_remote_config_url(url):
     set_remote(Remote(config=Config(config={'mcurl': url})))
+
 
 def get_remote_config_url():
     return use_remote().mcurl
