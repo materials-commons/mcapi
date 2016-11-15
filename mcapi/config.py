86925b99fefa6a7655105f25042db42cd0563cc2
Terry Weymouth <weymouth@umich.edu>
2016-11-15 09:29:11 -0500

    fixed test_config and cleaned up Config class

--- mcapi/config.py
+++ mcapi/config.py
@@ -3,14 +3,16 @@ import os
 from os.path import join
 import json
 
+
 class Config(object):
 
-    def __init__(self,config_file_path=None, config_file_name="config.json", config={}):
-        if (not config_file_path):
+    def __init__(self, config_file_path=None, config_file_name="config.json", override_config=None):
+        if not override_config:
+            override_config = {}
+        if not config_file_path:
             user = getpass.getuser()
             config_file_path = join(os.path.expanduser('~' + user), '.materialscommons')
-        path = join(config_file_path,config_file_name)
-        config_file = join(config_file_path, 'config.json')
+        config_file = join(config_file_path, config_file_name)
 
         if os.path.exists(config_file):
             with open(config_file, 'r') as f:
@@ -21,6 +23,9 @@ class Config(object):
         self.config = config
         for key in config:
             self.config[key] = os.getenv(key, config[key])
+
+        for key in override_config:
+            self.config[key] = override_config[key]
 
         self.mcapikey = config['apikey']
         self.mcurl = config['mcurl']
