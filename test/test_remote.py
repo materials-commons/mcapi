d201f1273e7d99fe411852ffc6a5a472f73cafb3
Terry Weymouth <weymouth@umich.edu>
2016-11-17 09:47:45 -0500

    Refactored test_remote to use env varialble for base of dir for test config files

--- test/test_remote.py
+++ test/test_remote.py
@@ -1,5 +1,6 @@
 import unittest
 from os.path import exists
+from os import environ, path
 from mcapi import Config
 from mcapi import Remote
 
@@ -23,7 +24,7 @@ class TestRemote(unittest.TestCase):
 
     def test_with_config_settings(self):
         test_path = "test/path/for/config"
-        config = Config(config_file_path="test/test_config_data/", config_file_name="config.json")
+        config = Config(config_file_path=self.make_test_dir_path(), config_file_name="config.json")
         remote = Remote(config=config)
         self.assertIsNotNone(remote.make_url(test_path))
         self.assertIsNotNone(remote.make_url_v2(test_path))
@@ -31,3 +32,13 @@ class TestRemote(unittest.TestCase):
         self.assertTrue(config.mcurl in remote.make_url_v2(test_path))
         self.assertTrue(test_path in remote.make_url(test_path))
         self.assertTrue(test_path in remote.make_url_v2(test_path))
+
+
+    def make_test_dir_path(self):
+        self.assertTrue('TEST_DATA_DIR' in environ)
+        test_dir = path.abspath(environ['TEST_DATA_DIR'])
+        self.assertIsNotNone(test_dir)
+        self.assertTrue(path.isdir(test_dir))
+        test_dir = path.join(test_dir, 'test_config_data')
+        self.assertTrue(path.isdir(test_dir))
+        return test_dir
