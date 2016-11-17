f421f8af8f2e51c1bc78f8481a380f8a7de412e7
Terry Weymouth <weymouth@umich.edu>
2016-11-17 08:17:47 -0500

    Refactored test_config to use environment varialble TEST_DATA_DIR as base for test data, specificially 'test_config_data/config.json'

--- test/test_config.py
+++ test/test_config.py
@@ -1,8 +1,12 @@
 import unittest
+from os import environ
 from mcapi import Config
 
 
 class TestConfig(unittest.TestCase):
+
+    def test_is_setup_correctly(self):
+        self.assertTrue('TEST_DATA_DIR' in environ)
 
     def test_default_config(self):
         config = Config()
@@ -12,10 +16,14 @@ class TestConfig(unittest.TestCase):
         self.assertIsNotNone(config.mcurl)
 
     def test_path_settings(self):
-        config = Config(config_file_path="test/test_config_data/", config_file_name="config.json")
+        test_dir = environ['TEST_DATA_DIR']
+        if not test_dir.endswith('/'):
+            test_dir += '/'
+        config = Config(config_file_path=test_dir + "test_config_data/", config_file_name="config.json")
         self.assertIsNotNone(config)
         self.assertIsNotNone(config.params)
         self.assertIsNotNone(config.params['apikey'])
         self.assertIsNotNone(config.mcurl)
         self.assertEqual(config.params['apikey'], "12345678901234567890123456789012")
         self.assertEqual(config.mcurl, "http://mctest.localhost/api")
+
