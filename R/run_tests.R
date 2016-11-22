382ae2024c782262506ac559d3e7feb244264750
Terry Weymouth <weymouth@umich.edu>
2016-11-22 14:23:28 -0500

    TDD - early bits of R version, config file fetch

--- R/run_tests.R
+++ R/run_tests.R
@@ -1,9 +1,12 @@
 # install.packages("RUnit", repos="http://cran.r-project.org")
 library('RUnit')
+# install.packages("testthat", repos="http://cran.r-project.org")
+library("testthat")
 
 # Note: assuming that load('run_tests.R') from top level directory
 # load the files that contain functions to be tested
 source('mcapi/hello.R')
+source('mcapi/config.R')
 
 # defile test suite ...
 test.suite <- defineTestSuite("hello",
