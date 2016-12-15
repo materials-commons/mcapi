2a4f76a2bbe0cf9fabd702fa27c41a5861eea670
Terry Weymouth <weymouth@umich.edu>
2016-12-15 10:17:45 -0500

    Updated both python/test/test_workflow.py and python/example/workflow.py

--- python/examples/workflow.py
+++ python/examples/workflow.py
@@ -1,36 +1,129 @@
 from random import randint
-from mcapi import create_project, Template, get_remote_config_url
-
-print "This example is running on the REST interface at " + get_remote_config_url()
-print "Using the configuration (config.json) in ~/.materialscommons/"
+from os import path as os_path
+from os import environ
+import sys
+from mcapi import create_project, Template, get_remote_config_url, get_process_from_id
 
 def fake_name(prefix):
     number="%05d" % randint(0,99999)
     return prefix+number
 
-project_name = "API-Project"
-experiment_name = fake_name("Experiment-")
+def make_test_dir_path(file_name):
+    path = os_path.abspath(environ['EXAMPLE_DATA_DIR'])
+    file = os_path.join(path, file_name)
+    return file
 
-print "Creating experiment, " + experiment_name + " in project " + project_name
 
+# fail early, if possible
+if not 'EXAMPLE_DATA_DIR' in environ:
+    print "This program requires that the environment variable EXAMPLE_DATA_DIR",\
+        "be set to the path of the directory for the test files"
+    sys.exit()
+
+test_path = os_path.abspath(environ['EXAMPLE_DATA_DIR'])
+if not os_path.isdir(test_path):
+    print "The environment variable EXAMPLE_DATA_DIR is set to", test_path,\
+        "which is not a directory, it must be the directory for the test files"
+    sys.exit()
+
+test_file1 = 'sem.tif'
+filepath1 = make_test_dir_path(test_file1)
+test_file2 = 'fractal.jpg'
+filepath2 = make_test_dir_path(test_file2)
+if not (os_path.isfile(filepath1) and os_path.isfile(filepath2)):
+    print "The environment variable EXAMPLE_DATA_DIR is set to", test_path,\
+        "and must contain the two test files:", test_file1, "and", test_file2,\
+        "-- However, at least one of them is missing"
+    sys.exit()
+
+# start
+print "This example is running on the REST interface at " + get_remote_config_url()
+print "Using the configuration (config.json) in ~/.materialscommons/"
+
+print "---- start ----"
+project_name = "Workflow Project - example"
 project_description = "a test project generated from api"
-experiment_description = "a test experiment generated from api"
-sample_name = 'Test Sample 1'
-process1_name = "Create Simulation Sample"
-process2_name = "Monte Carlo Simulation"
-
-## the workflow ##
 project = create_project(
     name=project_name,
     description=project_description)
+print "Created or reused project:", project_name
 
+experiment_name = fake_name("Experiment-")
+experiment_description = "a test experiment generated from api"
 experiment = project.create_experiment(
     name=experiment_name,
     description=experiment_description)
+print "Created experiment:", experiment_name
 
 create_sample_process = experiment.create_process_from_template(Template.create)
-sample = create_sample_process.create_samples([sample_name])[0]
 
+sample_name = 'Test Sample 1'
+sample = create_sample_process.create_samples(
+    sample_names=[sample_name]
+)[0]
+
+filepath_for_sample = make_test_dir_path(test_file1)
+filename_for_sample = "SampleFile.tif"
+sample_file = project.add_file_using_directory(
+    project.add_directory("/FilesForSample"),
+    filename_for_sample,
+    filepath_for_sample
+)
+create_sample_process.add_files([sample_file])
+create_sample_process = get_process_from_id(project,experiment,create_sample_process.id)
+print "Created a 'Create Sample' process with sample: ", sample_name
+print "   and attached file:", filename_for_sample," in directory /FilesForSample"
+
+# process2_name = "Monte Carlo Simulation"
 compute_process = experiment. \
     create_process_from_template(Template.compute). \
     add_samples_to_process([sample])
+
+compute_process.set_value_of_setup_property('number_of_processors',5)
+compute_process.set_value_of_setup_property('memory_per_processor',16)
+compute_process.set_unit_of_setup_property('memory_per_processor','gb')
+compute_process.set_value_of_setup_property('walltime',12)
+compute_process.set_unit_of_setup_property('walltime','h')
+compute_process.set_value_of_setup_property('submit_script',"exec.sh")
+compute_process = compute_process.update_setup_properties([
+        'number_of_processors','memory_per_processor','walltime','submit_script'
+])
+
+filepath_for_compute = make_test_dir_path(test_file2)
+filename_for_compute = "ResultsFile.jpg"
+compute_file = project.add_file_using_directory(
+    project.add_directory("/FilesForCompute"),
+    filename_for_compute,
+    filepath_for_compute
+)
+compute_process.add_files([compute_file])
+compute_process = get_process_from_id(project,experiment,compute_process.id)
+print "Created a 'Compute' process with attached file:", filename_for_compute," in directory /FilesForCompute"
+print "    and properties of: number of processors = 5, memory per processor = 16gb,"
+print "    wall time = 12h, and the submit script = 'exec.sh' "
+
+measurement_data = {
+    "name":"Composition",
+    "attribute":"composition",
+    "otype":"composition",
+    "unit":"at%",
+    "value":[
+        {"element":"Al","value":94},
+        {"element":"Ca","value":1},
+        {"element":"Zr","value":5}],
+    "is_best_measure":True
+}
+measurement = create_sample_process.create_measurement(data=measurement_data)
+
+measurement_property = {
+            "name":"Composition",
+            "attribute":"composition"
+        }
+create_sample_process_updated = \
+    create_sample_process.\
+    set_measurements_for_process_samples(\
+        measurement_property, [measurement])
+print "Added a Composition measurement to the 'Create Sample' process's samples with:"
+print "  Al at 94%, Ca at 1%, and Zr at 5%"
+
+print "---- done ----"
--- python/test/test_workflow.py
+++ python/test/test_workflow.py
@@ -23,35 +23,30 @@ class TestWorkflow(unittest.TestCase):
         self.assertEqual(get_remote_config_url(), url)
 
     def test_workflow(self):
-        project_name = "Workflow Project - test001"
-        project_description = "a test project generated from api"
-        experiment_name = fake_name("Experiment-")
-        experiment_description = "a test experiment generated from api"
-        sample_name = 'Test Sample 1'
-        # process1_name = "Create Simulation Sample"
-        # process2_name = "Monte Carlo Simulation"
-        # filepath_for_sample = 'test/test_upload_data/sem.tif'
-        # filepath_for_compute = 'test/test_upload_data/fractal.jpg'
-        filepath_for_sample = self.make_test_dir_path('sem.tif')
-        filepath_for_compute = self.make_test_dir_path('fractal.jpg')
-        filename_for_sample = "SampleFile.tif"
-        filename_for_compute = "ResultsFile.jpg"
 
         # #-# the workflow #-#
+        project_name = "Workflow Project - test001"
+        project_description = "a test project generated from api"
         project = create_project(
             name=project_name,
             description=project_description)
 
+        experiment_name = fake_name("Experiment-")
+        experiment_description = "a test experiment generated from api"
         experiment = project.create_experiment(
             name=experiment_name,
             description=experiment_description)
 
+        # process1_name = "Create Simulation Sample"
         create_sample_process = experiment.create_process_from_template(Template.create)
 
+        sample_name = 'Test Sample 1'
         sample = create_sample_process.create_samples(
             sample_names=[sample_name]
         )[0]
 
+        filepath_for_sample = self.make_test_dir_path('sem.tif')
+        filename_for_sample = "SampleFile.tif"
         sample_file = project.add_file_using_directory(
             project.add_directory("/FilesForSample"),
             filename_for_sample,
@@ -60,6 +55,7 @@ class TestWorkflow(unittest.TestCase):
         create_sample_process.add_files([sample_file])
         create_sample_process = get_process_from_id(project,experiment,create_sample_process.id)
 
+        # process2_name = "Monte Carlo Simulation"
         compute_process = experiment. \
             create_process_from_template(Template.compute). \
             add_samples_to_process([sample])
@@ -74,6 +70,8 @@ class TestWorkflow(unittest.TestCase):
                 'number_of_processors','memory_per_processor','walltime','submit_script'
         ])
 
+        filepath_for_compute = self.make_test_dir_path('fractal.jpg')
+        filename_for_compute = "ResultsFile.jpg"
         compute_file = project.add_file_using_directory(
             project.add_directory("/FilesForCompute"),
             filename_for_compute,
@@ -82,7 +80,29 @@ class TestWorkflow(unittest.TestCase):
         compute_process.add_files([compute_file])
         compute_process = get_process_from_id(project,experiment,compute_process.id)
 
-        # -# tests #-#
+        measurement_data = {
+            "name":"Composition",
+            "attribute":"composition",
+            "otype":"composition",
+            "unit":"at%",
+            "value":[
+                {"element":"Al","value":94},
+                {"element":"Ca","value":1},
+                {"element":"Zr","value":5}],
+            "is_best_measure":True
+        }
+        measurement = create_sample_process.create_measurement(data=measurement_data)
+
+        measurement_property = {
+                    "name":"Composition",
+                    "attribute":"composition"
+                }
+        create_sample_process_updated = \
+            create_sample_process.\
+            set_measurements_for_process_samples(\
+                measurement_property, [measurement])
+
+        # #-# tests #-# #
         self.assertIsNotNone(project.id)
         self.assertEqual(project_name, project.name)
         self.assertTrue(project_description in project.description)
@@ -142,6 +162,22 @@ class TestWorkflow(unittest.TestCase):
         self.assertIsNotNone(file2.id)
         self.assertEqual(file2.id, compute_file.id)
 
+        measurement_out = create_sample_process_updated.measurements[0]
+        self.assertEqual(measurement_out.name,measurement.name)
+        composition = measurement_out
+        self.assertEqual(composition.name,"Composition")
+        self.assertEqual(composition.attribute,"composition")
+        self.assertEqual(composition.otype,"composition")
+        self.assertEqual(composition.unit,"at%")
+        value_list = composition.value
+        self.assertEqual(value_list[0]['element'],"Al")
+        self.assertEqual(value_list[0]['value'],94)
+        self.assertEqual(value_list[1]['element'],"Ca")
+        self.assertEqual(value_list[1]['value'],1)
+        self.assertEqual(value_list[2]['element'],"Zr")
+        self.assertEqual(value_list[2]['value'],5)
+
+
     def make_test_dir_path(self, file_name):
         self.assertTrue('TEST_DATA_DIR' in environ)
         test_path = os_path.abspath(environ['TEST_DATA_DIR'])
@@ -150,10 +186,3 @@ class TestWorkflow(unittest.TestCase):
         test_file = os_path.join(test_path, 'test_upload_data', file_name)
         self.assertTrue(os_path.isfile(test_file))
         return test_file
-
-    def setup_test_file(self):
-        if not hasattr(self, 'filepath1'):
-            self.filepath1 = self.make_test_dir_path('fractal.jpg')
-            self.filepath2 = self.make_test_dir_path('sem.tif')
-            self.assertTrue(Path(self.filepath1).is_file())
-            self.assertTrue(Path(self.filepath2).is_file())
