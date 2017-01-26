695006f5aba263a201b5a0af07c5212ae2cfa85c
Terry Weymouth <weymouth@umich.edu>
2017-01-26 11:29:53 -0500

    added Template object and test; still debugging travis.yml

--- python/mcapi/__init__.py
+++ python/mcapi/__init__.py
@@ -2,6 +2,7 @@ from api import set_remote_config_url,get_remote_config_url
 from mc import Project, Experiment, Process, Sample, Template, Directory, File
 from mc import list_projects, create_project
 from mc import get_process_from_id
+from mc import get_all_templates
 
 # NOTE: the following exports are for debugging/testing only
 # Prefer to use top level functions (above) or class methods
