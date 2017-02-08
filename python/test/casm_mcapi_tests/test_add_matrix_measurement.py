844c334aa1fa2b75a12688166618d39206ff59af
Terry Weymouth <weymouth@umich.edu>
2017-02-08 13:01:12 -0500

    Setting up to test an extended example of measurement insertion - not working yet

--- python/test/casm_mcapi_tests/test_add_matrix_measurement.py
+++ python/test/casm_mcapi_tests/test_add_matrix_measurement.py
@@ -1,10 +1,9 @@
 import unittest
 import numpy as np
-import pytest
 from random import randint
 from mcapi import set_remote_config_url
 from mcapi import create_project, Template
-from casm_mcapi import _add_numpy_matrix_measurement
+from casm_mcapi import _add_nampy_matrix_measurement
 
 url = 'http://mctest.localhost/api'
 
@@ -93,7 +92,7 @@ class TestAddMatrixMeasurements(unittest.TestCase):
         value = np.array([[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]])
         name = "Lattice"
         type = "lattice"
-        process = _add_numpy_matrix_measurement(
+        process = _add_nampy_matrix_measurement(
             self.process, type, value, name=name)
         sample_out = process.output_samples[0]
         properties_out = sample_out.properties
