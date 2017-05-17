d7b1f215222c16109c0efee6c7532d4db535cdd3
bpuchala <bpuchala@users.noreply.github.com>
2017-05-17 09:49:33 -0400

    replace 'casm_mcapi' because it is needed for tests

---
+++ python/casm_mcapi/cli.py
@@ -0,0 +1,26 @@
+import argparse
+import sys
+
+casm_name = "casm"
+casm_desc = "Create CASM samples, processes, measurements, etc."
+
+prim_help = "Upload CASM Primitive Crystal Structure sample"
+composition_help = "Upload CASM Composition Axes sample"
+clex_help = "Upload CASM Cluster Expansion sample"
+mc_help = "Upload CASM Monte Carlo process"
+
+def casm_subcommand():
+    
+    parser = argparse.ArgumentParser(description = 'Upload CASM data to Materials Commons')
+    parser.add_argument('--prim', help=prim_help, action="store_true", default=False)
+    parser.add_argument('--composition', help=composition_help, action="store_true", default=False)
+    parser.add_argument('--clex', help=clex_help, action="store_true", default=False)
+    parser.add_argument('--mc', help=mc_help, action="store_true", default=False)
+    
+    # ignore 'mc casm'
+    args = parser.parse_args(sys.argv[2:])
+    
+    print "mc casm ...do something..."
+    
+    
+
