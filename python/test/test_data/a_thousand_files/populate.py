1ee257ae3ba056cc2a05bf025ac76002972aaeab
Terry Weymouth <weymouth@umich.edu>
2017-06-13 09:33:03 -0400

    code to generate test data for stress test - uploading a large number of files

---
+++ python/test/test_data/a_thousand_files/populate.py
@@ -0,0 +1,20 @@
+
+
+def populate():
+    for i in range(1000):
+        padding = ""
+        if (i < 1000): padding += "0"
+        if (i < 100):  padding += "0"
+        if (i < 10):   padding += "0"
+        filename = "testFile" + padding + "%d" % i + ".txt"
+        f = open(filename, "w+")
+        f.write("This is line of text in file %s\n" % filename)
+        f.close()
+
+
+def main():
+    populate()
+
+
+if __name__ == '__main__':
+    main()
