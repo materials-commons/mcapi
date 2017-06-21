468b4937b925762221f696b0413a7a517b15e7e8
Terry Weymouth <weymouth@umich.edu>
2017-06-21 09:52:35 -0400

    snapshot=setaside to do other work: testing upload-file delete

--- python/process_create_scrpits/load_EPMA_Melt5b.py
+++ python/process_create_scrpits/load_EPMA_Melt5b.py
@@ -2,6 +2,7 @@ import openpyxl
 import datetime
 from mcapi import create_project, get_all_templates
 
+
 # Excel Spreadsheet, from Tracy: ./input_data/EPMA_Melt5b_MC_Demo.xlsx
 # meaning of values are hardcoded
 # A - ignore
@@ -23,7 +24,47 @@ from mcapi import create_project, get_all_templates
 
 
 class WorkflowBuilder:
-    def parse_sheet(self,sheet):
+    def parse_sheet(self, sheet):
+        workflow_list = self.build_workflow_list(sheet)
+
+        self.create_project_experiment()
+        self.template_table = self.make_template_table()
+
+        self.create_processes = {}
+        self.section_processes = {}
+
+        for workflow in workflow_list:
+            self.create_process_name = 'Create ' + workflow["material"]
+            self.create_process_output_sample_name = workflow["material"]
+            self.section_process_name = 'Section ' + workflow["material"]
+            self.section_process_output_sample_name = workflow["material"] + "::" + workflow['geometry']
+            self.epma_process_name = 'EPMA - ' \
+                                + workflow['geometry'] + ' - ' + workflow['location']
+
+            self.add_create_sample_process_if_needed()
+            self.add_sectioning_process_if_needed()
+            epma_process = self.add_measurement_process()
+
+            epma_process = self.ready_process_for_additions(epma_process)
+
+            self.add_setup(epma_process,workflow['location'])
+            #self.add_measurements_and_annotations(epma_process,workflow)
+            #self.add_data_files(epma_process,workflow['data_file'])
+
+        print self.project.name
+
+    # build methods
+
+    def create_project_experiment(self):
+        time_stamp = '%s' % datetime.datetime.now()
+        project_name = "Example-From-Script: " + time_stamp
+        project_description = "Example workflow, created from script on " + time_stamp
+        self.project = create_project(project_name, project_description)
+
+        self.experiment = self.project.create_experiment("EPMA-Melt5b", "EPMA measurements of Melt5b samples")
+
+
+    def build_workflow_list(self,sheet):
         workflow_list = []
         for row in sheet.iter_rows(min_row=2):
             values = []
@@ -48,120 +89,101 @@ class WorkflowBuilder:
                     "data_file": values[17]
                 }
             )
+        return workflow_list
 
+    def add_create_sample_process_if_needed(self):
+        if not self.create_process_name in self.create_processes:
+            template_id = self.template_id_with(self.template_table, 'Create Samples')
+            process = self.experiment.create_process_from_template(template_id)
+            process.rename(self.create_process_name)
+            samples = process.create_samples(sample_names=[self.create_process_output_sample_name])
+            self.create_processes[self.create_process_name] = {
+                'process': process,
+                'sample': samples[0]
+            }
 
-        time_stamp = '%s' % datetime.datetime.now()
-        project_name = "Example-From-Script: " + time_stamp
-        project_description = "Example workflow, created from script on " + time_stamp
-        project = create_project(project_name,project_description)
+    def add_sectioning_process_if_needed(self):
+        if not self.section_process_name in self.section_processes:
+            template_id = self.template_id_with(self.template_table, 'Sectioning')
+            process = self.experiment.create_process_from_template(template_id)
+            process.rename(self.section_process_name)
+            samples = process.create_samples(sample_names=[self.section_process_output_sample_name])
+            process_and_sample = self.create_processes[self.create_process_name]
+            create_sample_output = process_and_sample['sample']
+            process.add_input_samples_to_process([create_sample_output])
+            section_output_samples = {}
+            section_output_samples[self.section_process_output_sample_name] = samples[0]
+            self.section_processes[self.section_process_name] = {
+                'process': process,
+                'samples': section_output_samples
+            }
+        else:
+            process_and_samples = self.section_processes[self.section_process_name]
+            samples = process_and_samples['samples']
+            if not self.section_process_output_sample_name in samples:
+                process = process_and_samples['process']
+                section_samples = process.create_samples(sample_names=[self.section_process_output_sample_name])
+                process_and_samples['samples'][self.section_process_output_sample_name] = section_samples[0]
 
-        experiment = project.create_experiment("EPMA-Melt5b","EPMA measurements of Melt5b samples")
+    def ready_process_for_additions(self,process):
+        self.experiment = self.experiment.decorate_with_processes()
+        found = process
+        for probe in self.experiment.processes:
+            if probe.id == process.id:
+                found = probe
+        return found
 
-        template_table = self._make_template_table()
+    def add_measurement_process(self):
+        process_and_samples = self.section_processes[self.section_process_name]
+        measurement_input_sample = process_and_samples['samples'][self.section_process_output_sample_name]
+        template_id = self.template_id_with(self.template_table, 'EPMA')
+        process = self.experiment.create_process_from_template(template_id)
+        process.rename(self.epma_process_name)
+        process.add_input_samples_to_process([measurement_input_sample])
+        return process
 
-        create_processes = {}
-        section_processes = {}
+    def add_setup(self,epma_process,location):
+        epma_process.set_value_of_setup_property('voltage', 10)
+        epma_process.set_unit_of_setup_property('voltage', 'kV')
+        epma_process.set_value_of_setup_property('beam_current', 10)
+        epma_process.set_unit_of_setup_property('beam_current', 'nA')
+        epma_process.set_value_of_setup_property('beam_size', 0)
+        scan_type = 'grid'
+        if location == 'edge':
+            scan_type = 'line'
+        epma_process.set_value_of_setup_property('scan_type', scan_type)
+        epma_process.set_value_of_setup_property('step_size', 10)
+        epma_process.set_value_of_setup_property('grid_dimensions', '20 x 20')
+        epma_process.set_value_of_setup_property('location', location)
+        epma_process.update_setup_properties([
+            'voltage', 'beam_current', 'beam_size', 'scan_type', 'step_size', 'grid_dimensions','location'
+        ])
 
-        for workflow in workflow_list:
+    def add_measurements_and_annotations(self,epma_process,workflow):
+        pass
 
-            create_process_name = 'Create ' + workflow["material"]
-            create_process_output_sample_name = workflow["material"]
-            section_process_name = 'Section ' + workflow["material"]
-            section_process_output_sample_name = workflow["material"] + "::" + workflow['geometry']
-            epma_process_name = 'EPMA Data Collection - ' \
-                                + workflow['geometry'] + ' - ' + workflow['location']
+    def add_data_files(self,epma_process, name):
+        pass
 
-            if not create_process_name in create_processes:
-                template_id = self._template_id_with(template_table, 'Create Samples')
-                process = experiment.create_process_from_template(template_id)
-                process.rename(create_process_name)
-                samples = process.create_samples(sample_names=[create_process_output_sample_name])
-                create_processes[create_process_name] = {
-                    'process' : process,
-                    'sample' : samples[0]
-                }
-
-            measurement_input_sample = None
-            if not section_process_name in section_processes:
-                template_id = self._template_id_with(template_table, 'Sectioning')
-                process = experiment.create_process_from_template(template_id)
-                process.rename(section_process_name)
-                samples = process.create_samples(sample_names=[section_process_output_sample_name])
-                process_and_sample = create_processes[create_process_name]
-                create_sample_output = process_and_sample['sample']
-                process.add_input_samples_to_process([create_sample_output])
-                section_output_samples = {}
-                section_output_samples[section_process_output_sample_name] = samples[0]
-                section_processes[section_process_name] = {
-                    'process': process,
-                    'samples': section_output_samples
-                }
-                measurement_input_sample = samples[0]
-            else:
-                process_and_samples = section_processes[section_process_name]
-                samples = process_and_samples['samples']
-                if not section_process_output_sample_name in samples:
-                    process = process_and_samples['process']
-                    section_samples = process.create_samples(sample_names=[section_process_output_sample_name])
-                    process_and_samples['samples'][section_process_output_sample_name] = section_samples[0]
-                measurement_input_sample = process_and_samples['samples'][section_process_output_sample_name]
-
-            template_id = self._template_id_with(template_table, 'EPMA')
-            process = experiment.create_process_from_template(template_id)
-            process.rename(epma_process_name)
-            process.add_input_samples_to_process([measurement_input_sample])
-
-        print project_name
-
-    # Support methods
-
-    def _make_template_table(self):
+    def make_template_table(self):
         template_list = get_all_templates()
         table = {}
         for template in template_list:
             table[template.id] = template
         return table
 
-    def _template_id_with(self,table,match):
+    def template_id_with(self, table, match):
         found_id = None
         for key in table:
             if match in key:
                 found_id = key
         return found_id
 
-    def _get_or_create_process(self, experiment, process_name, template_id):
-        experiment = experiment.decorate_with_processes()
-        processes = experiment.processes
-        selected_process = None
-        for process in processes:
-            if template_id == process.template_id and process_name == process.name:
-                selected_process = process
-        process = selected_process
-        if not process:
-            process = experiment.create_process_from_template(template_id)
-            process.rename(process_name)
-        return process
-
-    def _get_or_create_output_sample_from_process(self, process, sample_names):
-        samples = process.output_samples
-        selected_samples = []
-        for sample in samples:
-            if sample.name in sample_names:
-                selected_samples.append(sample)
-        samples = selected_samples
-        if not samples:
-            samples = process.create_samples(
-                sample_names=sample_names
-            )
-        return samples
-
-
 def main():
-    wb = openpyxl.load_workbook(filename='input_data/EPMA_Melt5b_MC_Demo.xlsx',read_only=True)
+    wb = openpyxl.load_workbook(filename='input_data/EPMA_Melt5b_MC_Demo.xlsx', read_only=True)
     ws = wb['Sheet1']
     process_by_row_list = WorkflowBuilder().parse_sheet(ws)
 
 
 if __name__ == '__main__':
     main()
-
