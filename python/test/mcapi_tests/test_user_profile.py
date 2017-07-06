d9e41b287c3019f62e0bc52053147e10a1c74d8d
Terry Weymouth <weymouth@umich.edu>
2017-07-06 09:34:31 -0400

    Refactor user-profile to use new REST interface

--- python/test/mcapi_tests/test_user_profile.py
+++ python/test/mcapi_tests/test_user_profile.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 from random import randint
 from mcapi import set_remote_config_url, get_remote_config_url
 from mcapi import _storeInUserProfile as storeInUserProfile
@@ -42,5 +43,45 @@ class TestUserProfile(unittest.TestCase):
         self.assertEqual(probe, name)
         probe = clearFromUserProfile(user_id, name)
         self.assertIsNone(probe)
+        probe = getFromUserProfile(user_id, name)
+        self.assertIsNone(probe)
+
+    def test_cannot_access_others_profile(self):
+        name = fake_name("test value - ")
+        value = name
+        probe = storeInUserProfile(user_id,name, value)
+        self.assertIsNotNone(probe)
+        self.assertEqual(probe, name)
+        probe = getFromUserProfile(user_id,name)
+        self.assertIsNotNone(probe)
+        self.assertEqual(probe, name)
+
+        another_user_id = "another@test.mc"
+        with pytest.raises(Exception):
+            getFromUserProfile(another_user_id, name)
+
+        probe = clearFromUserProfile(user_id, name)
+        self.assertIsNotNone(probe)
+        self.assertEqual(probe, name)
+        probe = getFromUserProfile(user_id, name)
+        self.assertIsNone(probe)
+
+    def test_cannot_modify_others_profile(self):
+        name = fake_name("test value - ")
+        value = name
+        probe = storeInUserProfile(user_id,name, value)
+        self.assertIsNotNone(probe)
+        self.assertEqual(probe, name)
+        probe = getFromUserProfile(user_id,name)
+        self.assertIsNotNone(probe)
+        self.assertEqual(probe, name)
+
+        another_user_id = "another@test.mc"
+        with pytest.raises(Exception):
+            storeInUserProfile(another_user_id, name, value)
+
+        probe = clearFromUserProfile(user_id, name)
+        self.assertIsNotNone(probe)
+        self.assertEqual(probe, name)
         probe = getFromUserProfile(user_id, name)
         self.assertIsNone(probe)
