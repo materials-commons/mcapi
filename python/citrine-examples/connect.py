7456dd15d55c3f7e32ba22e05b8a2f0a35e6a2d2
Terry Weymouth <weymouth@umich.edu>
2017-09-07 13:37:54 -0400

    fixed typo

--- python/citrine-examples/connect.py
+++ python/citrine-examples/connect.py
@@ -5,5 +5,3 @@ from os import environ
 client = CitrinationClient(environ['CITRINATION_API_KEY'], 'https://citrination.com')
 response = client.create_data_set("Weymouth - Test dataset","A scrap dataset for testing - Terry E Weymouth")
 print response
-
-client.
