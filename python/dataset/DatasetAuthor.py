11b9803173b218b5ea152c96c373f69c3c3cb1d1
Terry Weymouth <weymouth@umich.edu>
2017-09-19 09:01:05 -0400

    New class to represent dataset authors

---
+++ python/dataset/DatasetAuthor.py
@@ -0,0 +1,6 @@
+class DatasetAuthor:
+
+    def __init__(self, data=data):
+        self.firstname = data['firstname']
+        self.lastname = data['lastname']
+        self.affiliation = data['affiliation']
