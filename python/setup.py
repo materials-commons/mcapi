56143d01f70ba7712a0e5f6d997679e09d3a59c8
Terry Weymouth <weymouth@umich.edu>
2017-10-13 15:14:30 -0400

    PIP setting for setup.py

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mcapi',
-    version='0.8',
+    version='0.8.1',
     description='The Materials Commons Python API',
     long_description="""This is an implementation of the Python API, an interface to
     the Materials Commons server. We assume that the reader has used (or is otherwise
@@ -13,8 +13,8 @@ setup(
     to use this API in that context""",
     url='https://materials-commons.github.io/python-api/',
     author='Materials Commons development team',
-    author_email='help@materialscommons.org',
-    liscense='MIT',
+    author_email='weymouth@umich.edu',
+    license='MIT',
     packages=['mcapi', 'mcapi.cli'],
     scripts=['scripts/mc'],
     classifiers=[
@@ -55,35 +55,26 @@ setup(
     long_description="""This is a companion package to the Materials Commons Python API""",
     url='https://materials-commons.github.io/python-api/',
     author='Materials Commons development team',
-    author_email='help@materialscommons.org',
-    liscense='MIT',
+    author_email='weymouth@umich.edu',
+    license='MIT',
     classifiers = [
         # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
+        #'Development Status :: 2 - Pre - Alpha',
         'Development Status :: 3 - Alpha',
+        #'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Laboratory Tools',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific / Engineering',
 
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        # Specify the Python versions you support here.
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3'
     ],
     keywords = 'materials science mc lift prisims',
     install_requires=['mcapi'],
-    data_files = [
-        ("demo_data_files", ['demo_project/demo_project_data'])
-    ]
+    include_package_data=True
 )
