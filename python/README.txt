8543ca0673a17ab4d7c26cc8b850e9f5473bd70a
Terry Weymouth <weymouth@umich.edu>
2017-10-20 10:12:22 -0400

    Refactor for psgutil-style namecpace packages

--- python/README.txt
+++
@@ -1,1 +0,0 @@
-Please see README.md for description of package.
--- python/setup.py
+++ python/setup.py
@@ -3,19 +3,22 @@
 from setuptools import setup
 
 setup(
-    name='mcapi',
-    version='0.8.1',
-    description='The Materials Commons Python API',
-    long_description="""This is an implementation of the Python API, an interface to
-    the Materials Commons server. We assume that the reader has used (or is otherwise
-    familiar with) the Materials Commons web site, https://materialscommons.org/ or a
-    similar site based on the materials commons rest-interface server, and intends
-    to use this API in that context""",
+    name='materials-commons',
+    version='0.5.0',
+    description='The Materials Commons tool set',
+    long_description="""This modules contains: (1) materials-commons.mcapi, the Materials
+    Commons Python API, an interface to the Materials Commons servers; (2) materials-commons.cli,
+    the Material Commons command line interface which is built on the API and provides
+    command line access to the Materials Commons servers; and (3) materials-commons.demo-project
+    which provides a tool for building a demo project in your account at a Materials Commons
+    servers. We assume that the reader has used (or is otherwise familiar with) the Materials 
+    Commons web site, https://materialscommons.org/ or a similar site based on the
+    materials commons REST-interface, and intends to use these tools in that context""",
     url='https://materials-commons.github.io/python-api/',
     author='Materials Commons development team',
     author_email='weymouth@umich.edu',
     license='MIT',
-    packages=['mcapi', 'mcapi.cli'],
+    packages=['materials-commons.api', 'materials-commons.cli', 'materials-commons.demo-project'],
     scripts=['scripts/mc'],
     classifiers=[
         # How mature is this project? Common values are
@@ -26,7 +29,8 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Laboratory Tools',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
@@ -36,45 +40,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
     keywords='materials science mc lift prisims',
     install_requires=["requests", "rethinkdb","pathlib", "numpy", "pandas",
                       "tabulate", "sortedcontainers"
                       ]
-)
-
-setup(
-    name="mc_demo_project",
-    version='0.1',
-    packages=['demo_project'],
-    description='Demo project builder for the Materials Commons Python API',
-    long_description="""This is a companion package to the Materials Commons Python API""",
-    url='https://materials-commons.github.io/python-api/',
-    author='Materials Commons development team',
-    author_email='weymouth@umich.edu',
-    license='MIT',
-    classifiers = [
-        # How mature is this project? Common values are
-        #'Development Status :: 2 - Pre - Alpha',
-        'Development Status :: 3 - Alpha',
-        #'Development Status :: 4 - Beta',
-        #'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific / Engineering',
-
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3'
-    ],
-    keywords = 'materials science mc lift prisims',
-    install_requires=['mcapi'],
-    include_package_data=True
 )
