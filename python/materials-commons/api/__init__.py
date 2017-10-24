de30cac10bd4810237299968e9af046a431cb9e7
Terry Weymouth <weymouth@umich.edu>
2017-10-24 15:40:30 -0400

    Clean up init files

--- python/materials-commons/api/__init__.py
+++ python/materials-commons/api/__init__.py
@@ -3,8 +3,9 @@ from .mc import get_all_projects, create_project, get_project_by_id
 from .mc import get_all_users
 from .mc import get_all_templates
 
-from .mc import Property, MeasuredProperty, NumberProperty, StringProperty, BooleanProperty, DateProperty
-from .mc import DateProperty, SelectionProperty, FunctionProperty, CompositionProperty, VectorProperty, MatrixProperty
+from .mc import Property, MeasuredProperty, NumberProperty, StringProperty, BooleanProperty
+from .mc import DateProperty, SelectionProperty, FunctionProperty, CompositionProperty
+from .mc import VectorProperty, MatrixProperty
 
 from .measurement import Measurement, MeasurementComposition, MeasurementString, MeasurementMatrix, \
     MeasurementVector, MeasurementSelection, MeasurementFile, MeasurementInteger, MeasurementBoolean, \
@@ -15,17 +16,9 @@ from .bulk_file_uploader import BulkFileUploader
 from .api import set_remote_config_url, get_remote_config_url
 
 # for testing only!
-from .config import Config
-from .remote import Remote
-from .api import set_remote, use_remote
+from .config import Config as _Config
+from .remote import Remote as _Remote
+from .api import set_remote as _set_remote, use_remote as _use_remote
 from .mc import _make_dir_tree_table, _create_new_tamplate, _update_template
 from .mc import _storeInUserProfile, _getFromUserProfile, _clearFromUserProfile
 from . import api as __api
-
-name='api'
-
-# __all__ = dir()
-__all__ = ['get_all_projects', 'create_project', 'get_project_by_id', 'get_all_users', 'get_all_templates',
-           'Project', 'Experiment', 'Process', 'Sample', 'Template', 'Directory', 'File',
-           'User', 'mc_object_utility',
-           'set_remote_config_url', 'get_remote_config_url']
--- python/materials-commons/cli/__init__.py
+++ python/materials-commons/cli/__init__.py
@@ -1,7 +1,5 @@
-from mcapi.cli.parser import CommonsCLIParser
-from mcapi.cli.list_objects import ListObjects
-
-name = 'cli'
+from .parser import CommonsCLIParser
+from .list_objects import ListObjects
 
 __all__ = [
     'CommonsCLIParser',
