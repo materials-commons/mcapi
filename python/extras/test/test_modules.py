a978a2da9ffd02f5c8128e87df07eb66778c6179
Terry Weymouth <weymouth@umich.edu>
2017-10-24 15:20:48 -0400

    Move demo-project into extras.

---
+++ python/extras/test/test_modules.py
@@ -0,0 +1,13 @@
+import pip
+import unittest
+import sys
+
+class TestModules(unittest.TestCase):
+    def test_modules(self):
+        print("")
+        print("----")
+        print(sys.path)
+        print("----")
+
+        for entry in sorted(["%s==%s" % (i.key, i.version) for i in pip.get_installed_distributions()]):
+            print(entry)
