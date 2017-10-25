95376164b68c23ea4d7a0cf2ffa1880db82159db
Terry Weymouth <weymouth@umich.edu>
2017-10-25 11:29:02 -0400

    [skip ci] refactor for new pip package layout

--- python/extras/citrine-examples/get_dataset.py
+++ python/extras/citrine-examples/get_dataset.py
@@ -1,11 +1,7 @@
 from os import environ
 from os import path as os_path
 from random import randint
-import sys, os.path
-demo_project_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-                    + '/demo_project/')
-sys.path.append(demo_project_dir)
-import demo_project as demo
+import demo_project.demo_project as demo
 # for test-only dataset - normally datasets are not created through API!
 from materials_commons.api import __api as api
 from materials_commons.api import get_project_by_id
