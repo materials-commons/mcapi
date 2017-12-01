cf8706874db5de9c8ce78ef7d53ba99abcc2de15
Terry Weymouth <weymouth@umich.edu>
2017-12-01 10:48:08 -0500

    Framework for process sweep

--- python/materials_commons/etl/input/build_project.py
+++ python/materials_commons/etl/input/build_project.py
@@ -2,28 +2,32 @@ import openpyxl
 import argparse
 import datetime
 import os.path
-from os import walk
 import sys
 from materials_commons.api import create_project, get_all_projects, get_all_templates
 
 local_path = '/Users/weymouth/Desktop/etl-input/'
 BASE_DIRECTORY = os.path.abspath(local_path)
 
+
 class BuildProjectExperiment:
     def build(self, sheet, data_path):
         self.sheet = sheet
         self.source = self._readEntireSheet(sheet)
 
+        self.data_path = data_path
+
         self._setNames()
         if (self.project_name):
-            print("Project: ",self.project_name)
+            print("Project name: ", self.project_name)
         else:
             print("No project name found; check format. Quiting.")
+            return
 
         if (self.experiment_name):
-            print("Experiment: ",self.experiment_name)
+            print("Experiment name:", self.experiment_name)
         else:
-            print("No project name found; check format. Quiting.")
+            print("No experiment name found; check format. Quiting.")
+            return
 
         self._setRowPositions()
         self._set_col_positions()
@@ -31,21 +35,46 @@ class BuildProjectExperiment:
         print(self.start_sweep_col)
         print(self.end_sweep_col)
 
+        if not self.description:
+            self.description = "Project from excel spreadsheet"
+        self.project = create_project(self.project_name, self.description)
+        self.experiment = self.project.create_experiment(self.experiment_name, "")
+
+        self.sweep()
+
+        print("Created project:", self.project.name)
+
+    def sweep(self):
+        self._makeTemplateTable()
+        col_index = self.start_sweep_col
+        while col_index < self.end_sweep_col:
+            process_name = self.source[0][col_index]
+            if process_name and str(process_name).startswith("PROC:"):
+                process_name = self._pruneEntry(process_name, "PROC:")
+                template_id = self._getTemplateIdFor(process_name)
+                if template_id:
+                    col_index = self.sweepProcess(template_id, col_index)
+            col_index += 1
+
+    def sweepProcess(self, template_id, col_index):
+        process = self.experiment.create_process_from_template(template_id)
+        print(template_id, process.experiment.project.name)
+        return col_index
+
+    def setDescription(self, description):
+        self.description = description
+
+    def _pruneEntry(self, entry, prefix):
+        entry = str(entry)
+        if entry.startswith(prefix):
+            entry = entry[len(prefix):].strip(" ").strip("'").strip('"')
+        else:
+            entry = None
+        return entry
+
     def _setNames(self):
-        self.project_name = ""
-        self.experiment_name = ""
-        text = self.source[0][0]
-        if len(text) > 6 and text.startswith("PROJ: "):
-            text = text[6:]
-            text = text.strip("'")
-            text = text.strip('"')
-            self.project_name = text
-        text = self.source[1][0]
-        if len(text) > 5 and text.startswith("EXP: "):
-            text = text[5:]
-            text = text.strip("'")
-            text = text.strip('"')
-            self.experiment_name = text
+        self.project_name = self._pruneEntry(self.source[0][0], "PROJ:")
+        self.experiment_name = self._pruneEntry(self.source[1][0], "EXP:")
 
     def _setRowPositions(self):
         self.header_end_row = 0
@@ -86,16 +115,29 @@ class BuildProjectExperiment:
             data.append(values)
         return data
 
+    def _makeTemplateTable(self):
+        template_list = get_all_templates()
+        table = {}
+        for template in template_list:
+            table[template.id] = template
+        self.template_table = table
+
+    def _getTemplateIdFor(self, match):
+        found_id = None
+        for key in self.template_table:
+            if match in key:
+                found_id = key
+        return found_id
+
+
 def main(args):
     wb = openpyxl.load_workbook(filename=args.input, read_only=True)
     ws = wb['EPMA Results (Original)']
     builder = BuildProjectExperiment()
+    builder.setDescription("Project from excel spreadsheet: " + args.input
+                           + "; using data from " + args.dir)
     builder.build(ws, args.dir)
 
-#    print("Built project(s)...")
-#    for project in builder.projects:
-#        print(project.name)
-    # builder.pp()
 
 if __name__ == '__main__':
     time_stamp = '%s' % datetime.datetime.now()
