90b1ee483b0e7e68e4eee4ce961997a26bbf632e
Terry Weymouth <weymouth@umich.edu>
2017-12-12 11:20:13 -0500

    Snapshot for transfer

--- python/materials_commons/etl/output/extract_spreadsheet.py
+++ python/materials_commons/etl/output/extract_spreadsheet.py
@@ -17,37 +17,45 @@ class ExtractExperimentSpreadsheet:
     def extract(self,experiment):
         pass
 
-def main(file_name,output_file_path):
-    wb = openpyxl.load_workbook(filename=input)
-    ws = wb['EPMA Results (Original)']
+def main(args):
     builder = ExtractExperimentSpreadsheet()
-
-    builder.read_entire_sheet(ws)
-    wb.close()
-    builder.set_description("Project from excel spreadsheet: " + input
-                            + "; using data from " + data_dir)
-    builder.build(data_dir)
-
+    if not builder.setUpSpreadsheet(args.dir, args.file):
+        print("Unable to create spreadsheet, " + args.file + ", in directory " + args.dir)
+        exit(-1)
+    builder.buildExperimentArray()
+    builder.writeSpreadsheet()
 
 if __name__ == '__main__':
     time_stamp = '%s' % datetime.datetime.now()
     default_output_dir_path = os.path.join(BASE_DIRECTORY)
+    default_file_name = "workflow.xlsx"
 
     argv = sys.argv
     parser = argparse.ArgumentParser(
-        description='Build a workflow from given (well formatted) Excel spreadsheet')
-    parser.add_argument('--dir', type=str, default='',
-                        help='Path to output directory - defaults to ' + default_output_dir_path)
+        description='Dump a project-experiment to a spreadsheer')
+    parser.add_argument('--project', type=str, help="Project name")
+    parser.add_argument('--experiment', type=str, help="Experiment name")
+    parser.add_argument('--dir', type=str, default=default_output_dir_path,
+                        help='Path to output directory')
+    parser.add_argument('--file', type=str, default=default_file_name)
     args = parser.parse_args(argv[1:])
 
-    if not args.input:
-        args.input = default_input_path
-    if not args.dir:
-        args.dir = default_data_path
-    args.input = os.path.abspath(args.input)
+    if not args.project:
+        print("Project name (--project) is required")
+        exit(-1)
+    if not args.experiment:
+        print("Experiment name (--experiment) is required")
+        exit(-1)
     args.dir = os.path.abspath(args.dir)
 
-    print("Path to input EXCEL file: " + args.input)
-    print("Path to data file directory: " + args.dir)
+    if not os.path.isdir(args.dir):
+        print("The given output file path, " + args.dir + ", is not a directory. Please fix.")
+        exit(-1)
 
-    main(args.input, args.dir)
+    if not args.file.endswith(".xlsx"):
+        file = args.file + ".xlsx"
+
+    print("Set up: Writing spreadsheet, " + args.file + ", to directory " + args.dir)
+    print("From experiment '" + args.experiment + "' in project '" + args.project + "'")
+
+    main(args)
