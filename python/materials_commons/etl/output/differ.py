aa27b9cebc58abf5152be44c7b88152fb62bb99e
Terry Weymouth <weymouth@umich.edu>
2018-02-15 17:05:17 -0500

    Snapshot, EOD

--- python/materials_commons/etl/output/differ.py
+++ python/materials_commons/etl/output/differ.py
@@ -9,8 +9,8 @@ import openpyxl
 from materials_commons.api import get_all_projects
 from materials_commons.etl.common.worksheet_data import read_entire_sheet
 from materials_commons.etl.input.metadata import Metadata
+from materials_commons.etl.common.util import _normalise_property_name
 from .meta_data_verify import MetadataVerification
-
 
 class Differ:
     def __init__(self):
@@ -24,6 +24,13 @@ class Differ:
     def compute_deltas(self):
         metadata = self.metadata
         process_table = metadata.process_table
+        print("--------------------")
+        process = process_table['36eef9fc-c07f-4180-943e-4935e3109a60']
+        print(process.input_data)
+        print("--------------------")
+        print(process.setup[0].properties[1].input_data)
+        print(process.setup[0].properties[1].value)
+        print("--------------------")
         data = self.input_data
         delta_list = []
         print("Computing deltas for experiment:", self.experiment.name)
@@ -94,15 +101,12 @@ class Differ:
         return True
 
     def set_up_input_data(self):
-        print("Setup input data")
         file_id = self.metadata.input_excel_file_id
-        print("Excel file id:", file_id)
         if not file_id:
             print("Experiment metadata does not contain id of excel file")
             return False
         with tempfile.TemporaryDirectory() as tmpdirname:
             directory = self.project.add_directory("/Input Excel Spreadsheets")
-            print(directory.name)
             file = None
             for child in directory.get_children():
                 if child.id == file_id:
@@ -112,15 +116,12 @@ class Differ:
                 print("Can not locate file for excel input")
                 return False
             else:
-                print(file.name)
                 local_file_path = str(Path(tmpdirname, file.name))
-                print(local_file_path)
                 file.download_file_content(local_file_path)
-                print(local_file_path, Path(local_file_path).exists())
                 wb = openpyxl.load_workbook(filename=local_file_path)
                 sheet_name = wb.sheetnames[0]
                 ws = wb[sheet_name]
-                print("In Excel file, using sheet", sheet_name, "from sheets", wb.sheetnames)
+                print("In input Excel file, using sheet", sheet_name, "from sheets", wb.sheetnames)
                 self.input_data = read_entire_sheet(ws)
                 wb.close()
         return True
@@ -128,7 +129,6 @@ class Differ:
     def _process_deltas(self):
         deltas = []
         if self.missing_process_list:
-            print("Processes that are in original input that are not in the experiment")
             for process_id in self.missing_process_list:
                 deltas.append({
                     "type": "missing_process",
@@ -137,7 +137,6 @@ class Differ:
                     }
                 })
         if self.added_process_list:
-            print("Processes that are not in original input that are in the experiment")
             for process_id in self.added_process_list:
                 deltas.append({
                     "type": "added_process",
@@ -148,10 +147,103 @@ class Differ:
         return deltas
 
     def _attribute_deltas(self):
-        return []
+        deltas = []
+        for process_id in self.metadata.process_table:
+            if process_id in self.missing_process_list:
+                continue
+            metadata_attribute_list = self._get_metadata_attributes(process_id)
+            process_attribute_list = self._getProcess_attributes(process_id)
+            for attribute in metadata_attribute_list:
+                if not attribute in process_attribute_list:
+                    deltas.append({
+                        "type": "missing_attribute",
+                        "data": {
+                            'process_id': process_id,
+                            'attribute': attribute
+                        }
+                    })
+            for attribute in process_attribute_list:
+                if not attribute in metadata_attribute_list:
+                    deltas.append({
+                        "type": "added_attribute",
+                        "data": {
+                            'process_id': process_id,
+                            'attribute': attribute
+                        }
+                    })
+        return deltas
 
     def _value_deltas(self):
         return []
+
+    def _get_metadata_attributes(self, process_id):
+        attribute_list = []
+        process_record = self._get_metadata_process_record(process_id)
+        types_row = self.input_data[self.metadata.start_attribute_row]
+        attributes_row = self._remove_units_spec_from_attributes(
+            self.input_data[self.metadata.start_attribute_row + 1])
+        attributes_row = self._make_normalized_attribute_names(attributes_row)
+        if process_record:
+            start_col = process_record['start_col']
+            end_col = process_record['end_col']
+            for col in range(start_col, end_col):
+                if self._is_attribute(types_row[col]):
+                    attribute_list.append(attributes_row[col])
+        print("  for process id", process_id, "metadata attributes are", attribute_list)
+        return attribute_list
+
+    def _getProcess_attributes(self, process_id):
+        attribute_list = []
+        process = self.metadata.process_table[process_id]
+        for s in process.setup:
+            for prop in s.properties:
+                print(process_id, prop.attribute, prop.value)
+                if (prop.value is not None) and (str(prop.value).strip() != ""):
+                    attribute_list.append(prop.attribute)
+        for m in process.measurements:
+            attribute_list.append(m.attribute)
+        print("  for process id", process_id, "process attributes are", attribute_list)
+        return attribute_list
+
+    def _get_metadata_process_record(self, process_id):
+        found = None
+        for record in self.metadata.process_metadata:
+            if process_id == record['id']:
+                found = record
+                break
+        return found
+
+    def _is_attribute(self, attribute_type):
+        return attribute_type in ['PARAM','MEAS']
+
+    @staticmethod
+    def _remove_units_spec_from_attributes(attributes):
+        update = []
+        for attr in attributes:
+            if attr and '(' in attr:
+                pos = attr.find('(')
+                if pos > 2:
+                    attr = attr[0:pos - 1]
+            update.append(attr)
+        return update
+
+    @staticmethod
+    def _make_normalized_attribute_names(attributes):
+        update = []
+        for attr in attributes:
+            if attr:
+                if isinstance(attr, str):
+                    attr = _normalise_property_name(attr)
+                else:
+                    parts = []
+                    for part in attr:
+                        if not parts:
+                            # only normalize the leading term
+                            part = _normalise_property_name(part)
+                        parts.append(part)
+                    attr = parts
+            update.append(attr)
+        return update
 
 
 def main(project_name, experiment_name):
