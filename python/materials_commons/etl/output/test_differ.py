726ab517ed2b26750dd2f4009a1e0c8bd0504341
Terry Weymouth <weymouth@umich.edu>
2018-02-22 13:35:58 -0500

    Initial framework for loading excel spreadsheet with delta records

--- python/materials_commons/etl/output/test_differ.py
+++ python/materials_commons/etl/output/test_differ.py
@@ -6,12 +6,15 @@ from pathlib import Path
 from materials_commons.etl.input.build_project import BuildProjectExperiment
 from .differ import Differ
 from .modify_workflow import Modifier
+from .extract_spreadsheet_with_changes import ExtractExperimentSpreadsheetWithChanges
 
 
 class Tester:
-    def __init__(self, spread_sheet_path, data_dir):
+    def __init__(self, spread_sheet_path, output_file_path, upload_data_dir, download_data_dir):
         self.spread_sheet_path = spread_sheet_path
-        self.data_dir = data_dir
+        self.data_dir = upload_data_dir
+        self.output_file_path = output_file_path
+        self.download_data_dir = download_data_dir
         self.project = None
         self.experiment = None
 
@@ -60,6 +63,25 @@ class Tester:
             print("No differences were detected. Done")
         else:
             differ.report_deltas(deltas)
+        print("================== output ==========================")
+        print("================== output ==========================")
+        print("================== output ==========================")
+        output = self.output_file_path
+        builder = ExtractExperimentSpreadsheetWithChanges(output)
+        builder = register_deltas(deltas)
+        ok = builder.set_up_project_experiment_metadata(project_name, experiment_name)
+        download = self.download_data_dir
+        if not ok:
+            print("Invalid configuration of metadata or experiment/metadata mismatch. Quiting")
+        else:
+            print("Writing experiment '" + builder.experiment.name
+                  + "' in project, '" + builder.project.name + ", to")
+            print("spreadsheet at " + builder.output_path)
+            builder.build_experiment_array()
+            builder.write_spreadsheet()
+            if download:
+                print("Downloading process files to " + download)
+                builder.download_process_files(download)
 
     def verify_data_dir(self):
         path = Path(self.data_dir)
@@ -71,17 +93,33 @@ class Tester:
         ok = path.exists() and path.is_file()
         return ok
 
+    def verify_output_path(self):
+        path = Path(self.output_file_path)
+        dir = path.parent
+        print("This is parent dir of output file??: ",dir)
+        ok = dir.exists() and dir.is_dir()
+        return ok
 
-def main(spread_sheet_path, data_dir):
-    tester = Tester(spread_sheet_path, data_dir)
+    def verify_download_data_dir(self):
+        path = Path(self.download_data_dir)
+        ok = path.exists() and path.is_dir()
+        return ok
+
+def main(input_path, output_path, upload_dir, download_dir):
+    tester = Tester(input_path, output_path, upload_dir, download_dir)
     if not tester.verify_input_path():
         print("The Path to the input Excel spreadsheet does not point to a file; exiting.")
         exit(-1)
-    if data_dir and (not tester.verify_data_dir()):
-        print("The Path to data file directory does not point to a user directory; exiting.")
+    if not tester.verify_output_path():
+        print("The Path to the output Excel spreadsheet is not available; exiting.")
+        exit(-1)
+    if upload_dir and (not tester.verify_data_dir()):
+        print("The Path to the file file directory for upload does not point to a user directory; exiting.")
+        exit(-1)
+    if download_dir and (not tester.verify_download_data_dir()):
+        print("The Path to the file file directory for download does not point to a user directory; exiting.")
         exit(-1)
     tester.run()
-
 
 if __name__ == '__main__':
     argv = sys.argv
@@ -89,8 +127,13 @@ if __name__ == '__main__':
         description='Build a workflow from given (well formatted) Excel spreadsheet')
     parser.add_argument('input', type=str,
                         help='Path to input EXCEL file')
+    parser.add_argument('output', type=str,
+                        help='Path to output EXCEL file')
     parser.add_argument('--upload', type=str,
                         help='Path to directory of data files to upload - optional')
+    parser.add_argument('--download', type=str,
+                        help="Path to dir for downloading files; if none, files are not downloaded")
+
     args = parser.parse_args(argv[1:])
 
     args.input = os.path.abspath(args.input)
@@ -102,4 +145,15 @@ if __name__ == '__main__':
     else:
         print("Path to directory as sources of file upload, not specified. File upload suppressed")
 
-    main(args.input, args.upload)
+    if args.download:
+        args.download = os.path.abspath(args.download)
+        print("Path to directory as sources of file upload: " + args.download)
+    else:
+        print("Path to directory as sources of file upload, not specified. File upload suppressed")
+
+
+    print("Output excel spreadsheet using path: " + args.output)
+    if args.download:
+        print("  and with file downloads going to " + args.download)
+
+    main(args.input, args.output, args.upload, args.download)
