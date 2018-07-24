14835ee0af5e33648461d2aa37b64a7112fcb381
Terry Weymouth <weymouth@umich.edu>
2018-07-24 08:02:34 -0400

    moving test scripts 'mulltiuser_check' to materialscommons.org

--- python/extras/test_scripts/mulltiuser_check/task_call.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-
-def task_call(worker):
-    main_log = logging.getLogger("task_call")
-    main_log.info("Starting worker {}".format(worker.name))
-    worker.run()
-    main_log.info("Finished worker {}".format(worker.name))
