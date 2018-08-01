fd563abd80359d6b09d05815679e9f1ae0294e35
Terry Weymouth <weymouth@umich.edu>
2018-08-01 11:08:39 -0400

    commented out version test

--- python/materials_commons_extras/test/mcapi_tests/test_version.py
+++ python/materials_commons_extras/test/mcapi_tests/test_version.py
@@ -6,7 +6,7 @@ import pkg_resources
 
 class TestVersion(unittest.TestCase):
 
-    # @pytest.mark.skip("Skipping TestVersion::test_version - does not work when materials_commons is not installed")
+    @pytest.mark.skip("Skipping TestVersion::test_version - does not work when materials_commons is not installed")
     def test_version(self):
         print("Code version is ",version.version())
         with open(pkg_resources.resource_filename('materials_commons', 'VERSION.txt')) as f:
