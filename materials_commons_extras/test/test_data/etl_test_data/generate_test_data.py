6f6fc525ec44e1007acd4a2f0557629dda34ee36
gtarcea <glenn.tarcea@gmail.com>
2019-01-17 12:06:02 -0500

    Turn into a script

--- materials_commons_extras/test/test_data/etl_test_data/generate_test_data.py
+++ materials_commons_extras/test/test_data/etl_test_data/generate_test_data.py
@@ -1,44 +1,47 @@
+#!/usr/bin/env python
 import os
 
-names = [
-    "file1.txt",
-    "file2.txt",
-    "file3.txt",
-    "file4.txt",
-    "file5.txt",
-    "not_upload_file.txt",
-]
+if __name__ == "__main__":
 
-paths = [
-    'test_data/data/',
-    'test_data/data/NotUpload/',
-    'test_data/data/Otherdir/',
-    'test_data/data/results/',
-    'test_data/data/Subdir/'
-]
+    names = [
+        "file1.txt",
+        "file2.txt",
+        "file3.txt",
+        "file4.txt",
+        "file5.txt",
+        "not_upload_file.txt",
+    ]
 
-for path in paths:
-    os.makedirs(path)
+    paths = [
+        'test_data/data/',
+        'test_data/data/NotUpload/',
+        'test_data/data/Otherdir/',
+        'test_data/data/results/',
+        'test_data/data/Subdir/'
+    ]
 
-sets = [[0, 5], [4], [3], [], [1,2]]
+    for path in paths:
+        os.makedirs(path)
 
-for p in range(0, len(sets)):
-    path = paths[p]
-    set = sets[p]
-    for n in range(0, len(set)):
-        name = names[set[n]]
-        with open(path + name, 'w') as file:
+    sets = [[0, 5], [4], [3], [], [1,2]]
+
+    for p in range(0, len(sets)):
+        path = paths[p]
+        set = sets[p]
+        for n in range(0, len(set)):
+            name = names[set[n]]
+            with open(path + name, 'w') as file:
+                file.write("This is a test data file, " + name + "\n")
+    #            file.write("This is a test data file, " + name)
+                file.close()
+
+    for i in range(1,20):
+        tag = str(i)
+        if i < 10:
+            tag = "0" + tag
+        name = "DataFile" + tag + ".txt"
+        with open(paths[3] + name,'w') as file:
             file.write("This is a test data file, " + name + "\n")
-#            file.write("This is a test data file, " + name)
             file.close()
 
-for i in range(1,20):
-    tag = str(i)
-    if i < 10:
-        tag = "0" + tag
-    name = "DataFile" + tag + ".txt"
-    with open(paths[3] + name,'w') as file:
-        file.write("This is a test data file, " + name + "\n")
-        file.close()
-
-print("done.")
+    print("done.")
