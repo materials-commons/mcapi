5f277de70499685309f9bd99f8b35f6fe7934d2c
bpuchala <bpuchala@users.noreply.github.com>
2019-08-05 14:25:38 -0400

    init_project as own function

--- materials_commons/cli/init.py
+++ materials_commons/cli/init.py
@@ -7,6 +7,74 @@ import materials_commons.api as mcapi
 import materials_commons.cli.functions as clifuncs
 
 
+def init_project(name, description="", prefix=None, remote=None):
+    """Initialize directory prefix/name as a new project
+
+    Arguments
+    ---------
+    name: str, Project name. If prefix/name does not exist it will be created.
+    description: str, Project description.
+    prefix: str, The project directory will be created at prefix/name.
+    remote: mcapi.Remote, The remote where the project will be created.
+
+    Returns
+    -------
+    proj: mcapi.Project
+
+    Raises
+    ------
+    MCGenericException: If any of the following occur:
+        - prefix does not exist
+        - prefix/name is a file
+        - prefix/name/.mc already exists
+    """
+    if prefix is None:
+        prefix = os.getcwd()
+
+    if not os.path.exists(prefix):
+        raise mcapi.MCGenericException("Error in init_project: '" + prefix + "' does not exist.")
+
+    proj_path = os.path.join(prefix, name)
+
+    if os.path.isfile(proj_path):
+        raise mcapi.MCGenericException("Error in init_project: '" + proj_path + "' is a file.")
+
+    if os.path.exists(os.path.join(proj_path, ".mc")):
+        pconfig = clifuncs.read_project_config(proj_path)
+
+        # if .mc directory already exists, print error message
+        if pconfig:
+            try:
+                proj = clifuncs.make_local_project(proj_path)
+            except mcapi.MCNotFoundException as e:
+                print(e)
+                # print("A .mc directory already exists, but could not find existing project.")
+                # print("This may mean the project was deleted.")
+                # print("If you wish to create a new project here, first delete the .mc directory.")
+                s = "A .mc directory already exists, but could not find existing project.\n"
+                s += "This may mean the project was deleted.\n"
+                s += "If you wish to create a new project here, first delete the .mc directory.\n"
+                raise mcapi.MCGenericException(s)
+
+            # print("Already in project.  name:", proj.name, "  id:", proj.id)
+            raise mcapi.MCGenericException("Already in project.  name: " + proj.name + "   id: " + proj.id)
+    else:
+        if not os.path.exists(proj_path):
+            os.mkdir(proj_path)
+
+    # create new project
+    proj = clifuncs.create_project_or_exit(name, description, remote=remote)
+    proj.local_path = proj_path
+    proj.remote = remote
+
+    # create project config directory and file
+    pconfig = clifuncs.ProjectConfig(proj.local_path)
+    pconfig.remote = proj.remote.config
+    pconfig.project_id = proj.id
+    pconfig.save()
+
+    return proj
+
 def init_subcommand(argv=sys.argv):
     """
     Initialize a new project
@@ -23,37 +91,54 @@ def init_subcommand(argv=sys.argv):
     # ignore 'mc init'
     args = parser.parse_args(argv[2:])
 
-    proj_path = os.getcwd()
-    pconfig = clifuncs.read_project_config(proj_path)
-
-    # if .mc directory already exists, print error message
-    if pconfig:
-        try:
-            proj = clifuncs.make_local_project(proj_path)
-        except mcapi.MCNotFoundException as e:
-            print(e)
-            print("A .mc directory already exists, but could not find existing project.")
-            print("This may mean the project was deleted.")
-            print("If you wish to create a new project here, first delete the .mc directory.")
-            return
-
-        print("Already in project.  name:", proj.name, "  id:", proj.id)
-        return
+    # proj_path = os.getcwd()
+    # pconfig = clifuncs.read_project_config(proj_path)
+    #
+    # # if .mc directory already exists, print error message
+    # if pconfig:
+    #     try:
+    #         proj = clifuncs.make_local_project(proj_path)
+    #     except mcapi.MCNotFoundException as e:
+    #         print(e)
+    #         print("A .mc directory already exists, but could not find existing project.")
+    #         print("This may mean the project was deleted.")
+    #         print("If you wish to create a new project here, first delete the .mc directory.")
+    #         return
+    #
+    #     print("Already in project.  name:", proj.name, "  id:", proj.id)
+    #     return
+    #
+    # # get remote, from command line option or default
+    # remote = clifuncs.optional_remote(args)
+    #
+    # # create new project
+    # name = os.path.basename(proj_path)
+    # proj = clifuncs.create_project_or_exit(name, args.desc, remote=remote)
+    # proj.local_path = proj_path
+    # proj.remote = remote
+    #
+    # print("Created new project at:", remote.config.mcurl)
+    # clifuncs.print_projects([proj], proj)
+    #
+    # # create project config directory and file
+    # pconfig = clifuncs.ProjectConfig(proj.local_path)
+    # pconfig.remote = proj.remote.config
+    # pconfig.project_id = proj.id
+    # pconfig.save()
 
     # get remote, from command line option or default
     remote = clifuncs.optional_remote(args)
 
-    # create new project
+    proj_path = os.getcwd()
     name = os.path.basename(proj_path)
-    proj = clifuncs.create_project_or_exit(name, args.desc, remote=remote)
-    proj.local_path = proj_path
-    proj.remote = remote
+    prefix = os.path.dirname(proj_path)
+
+    try:
+        proj = init_project(name, args.desc, prefix=prefix, remote=remote)
+    except mcapi.MCGenericException as e:
+        print(e)
+        exit(1)
 
     print("Created new project at:", remote.config.mcurl)
     clifuncs.print_projects([proj], proj)
-
-    # create project config directory and file
-    pconfig = clifuncs.ProjectConfig(proj.local_path)
-    pconfig.remote = proj.remote.config
-    pconfig.project_id = proj.id
-    pconfig.save()
+    print("")
