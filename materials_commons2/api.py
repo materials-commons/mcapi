4754da76587176c88b13d49983be2ed24b1910ea
gtarcea <glenn.tarcea@gmail.com>
2020-01-17 10:37:44 -0500

    Start laying out v2 of api

---
+++ materials_commons2/api.py
@@ -0,0 +1,39 @@
+import requests
+
+
+def login(email, password):
+    pass
+
+# Project API
+
+
+def get_projects():
+    pass
+
+
+def create_project(name, description):
+    pass
+
+
+def delete_project(id):
+    pass
+
+# Files and Directories
+
+
+def create_directory(project_id, parent_dir_id, name):
+    pass
+
+
+def get_file_by_path(project_id, path):
+    pass
+
+
+def move_file(file_id, to_directory_id):
+    pass
+
+
+def move_directory(directory_id, to_directory_id):
+    pass
+
+
---
+++ materials_commons2/project.py
@@ -0,0 +1,6 @@
+
+class Project(object):
+
+    def __init__(self, project_attrs):
+        self.attrs = project_attrs
+        self.project_id = project_attrs['id']
