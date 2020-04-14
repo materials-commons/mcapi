b0db08d211d8e3418befa3b55925a83c5007b5ac
gtarcea <glenn.tarcea@gmail.com>
2020-04-14 13:59:25 -0400

    Add directory and file support

--- materials_commons2/client.py
+++ materials_commons2/client.py
@@ -3,6 +3,12 @@ from collections import OrderedDict
 import requests
 
 
+def merge_dicts(dict1, dict2):
+    merged = dict1.copy()
+    merged.update(dict2)
+    return merged
+
+
 class Client(object):
 
     def __init__(self, apikey, base_url="https://materialscommons.org/api"):
@@ -17,12 +23,9 @@ class Client(object):
     def get_projects(self, params=None):
         return self.get("/projects", params)
 
-    def create_project(self, name, summary=None, description=None):
+    def create_project(self, name, attrs={}):
         form = {"name": name}
-        if summary is not None:
-            form["summary"] = summary
-        if description is not None:
-            form["description"] = description
+        form = merge_dicts(form, attrs)
         data = self.post("/projects", form)
         self.set_base_project(data["data"]["id"])
         return data
@@ -33,16 +36,63 @@ class Client(object):
         return data
 
     def delete_project(self, project_id=None):
-        if project_id is None:
-            project_id = self.project_id
+        project_id = self._get_project_id(project_id)
         self.delete("/projects/" + str(project_id))
         if project_id == self.project_id:
             self.set_base_project(None)
 
-    def get_experiments(self):
-        if self.project_id is None:
-            return None
-        return self.get("/projects/" + self.project_id + "/experiments")
+    def update_project(self, attrs, project_id=None):
+        project_id = self._get_project_id(project_id)
+        return self.put("/projects" + str(project_id), attrs)
+
+    def get_directory(self, directory_id, params=None):
+        return self.get("/directories/" + str(directory_id), params)
+
+    def create_directory(self, name, parent_id, attrs):
+        form = {"name": name, "directory_id": parent_id, "project_id": self.project_id}
+        form = merge_dicts(form, attrs)
+        return self.post("/directories", form)
+
+    def move_directory(self, directory_id, to_directory_id):
+        form = {"to_directory_id": to_directory_id}
+        return self.post("/directories/" + str(directory_id) + "/move", form)
+
+    def rename_directory(self, directory_id, name):
+        form = {"name": name}
+        return self.post("/directories/" + str(directory_id) + "/rename", form)
+
+    def delete_directory(self, directory_id):
+        return self.delete("/directories/" + str(directory_id))
+
+    def update_directory(self, directory_id, attrs):
+        return self.put("/directories/" + str(directory_id), attrs)
+
+    def get_file(self, file_id, params):
+        return self.get("/files/" + str(file_id), params)
+
+    def update_file(self, file_id, attrs):
+        return self.put("/files/" + str(file_id), attrs)
+
+    def delete_file(self, file_id):
+        return self.delete("/files/" + str(file_id))
+
+    def move_file(self, file_id, to_directory_id):
+        form = {"directory_id": to_directory_id}
+        return self.post("/files/" + str(file_id) + "/move", form)
+
+    def rename_file(self, file_id, name):
+        form = {"name": name}
+        return self.post("/files/" + str(file_id) + "/rename", form)
+
+    def _get_project_id(self, project_id):
+        if project_id is None:
+            return self.project_id
+        return project_id
+
+    # def get_experiments(self):
+    #     if self.project_id is None:
+    #         return None
+    #     return self.get("/projects/" + self.project_id + "/experiments")
 
     def get(self, urlpart, params):
         url = self.base_url + urlpart
