5217e1af5814247d572c319c520d73749cc19a17
bpuchala <bpuchala@users.noreply.github.com>
2020-06-15 15:09:39 -0400

    updating path construction

--- materials_commons2_cli/file_functions.py
+++ materials_commons2_cli/file_functions.py
@@ -1,3 +1,5 @@
+import os.path
+import requests
 import materials_commons2 as mcapi
 from .exceptions import MCCLIException
 
@@ -15,3 +17,35 @@ def get_parent_id(file_or_dir):
         return file_or_dir.directory_id
     else:
         raise MCCLIException("file_or_dir is missing attribute directory_id")
+
+def make_local_abspath(proj_local_path, mcpath):
+    return os.path.join(proj_local_path, os.path.relpath(mcpath, "/"))
+
+def make_mcpath(proj_local_path, local_abspath):
+    """
+        :param str proj_local_path: Path to project, i.e. "/path/to/project"
+        :param str local_abspath: Path to file in project, i.e. "/path/to/project/path/to/file"
+        :return str mcpath: Returns a Materials Commons style path, i.e. "/path/to/file"
+    """
+    relpath = os.path.relpath(local_abspath, proj_local_path)
+    if relpath == ".":
+        mcpath = "/"
+    else:
+        mcpath = os.path.join("/", relpath)
+    return mcpath
+
+def get_by_path_if_exists(client, project_id, file_path):
+    """
+    Get file (or directory) by path in project, if it exists.
+    :param int project_id: The id of the project containing the file or directory
+    :param file_path: The path to the file or directory
+    :return: The file or None
+    :rtype File or None
+    """
+    try:
+        return client.get_file_by_path(project_id, file_path)
+    except requests.exceptions.HTTPError as e:
+        print("get_file_by_path error:", str(e))
+        if e.response.status_code == 404:
+            return None
+        raise e
