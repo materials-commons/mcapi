f5a55cdfd8ef0b75f3bbfd40aea92a6931f8906b
gtarcea <gtarcea@umich.edu>
2025-08-13 20:09:27 -0400

    But version

--- materials_commons/api/__init__.py
+++ materials_commons/api/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.0b5'
+__version__ = '2.0.1'
 from .client import Client, MCAPIError
 from .models import Project, Activity, Dataset, Entity, Experiment, File, User, Workflow, GlobusTransfer, GlobusUpload, \
     GlobusDownload, Paged
